b9b283363a1d96d90d21d81cff52376b50aa67f0
Erika Cuby <kubickova.erika@gmail.com>
2025-03-24 12:48:44 +0100

    Putted the carousel into an infinite loop

--- src/RelatedCarousel.js
+++ src/RelatedCarousel.js
@@ -1,49 +1,30 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import blogPosts from "./blogPosts";
 
 function RelatedCarousel({ currentPostId }) {
-  const carouselRef = useRef(null);
-
-  useEffect(() => {
-    const carousel = carouselRef.current;
-    let scrollAmount = 0;
-    const scrollStep = 1;
-
-    const autoScroll = () => {
-      if (!carousel) return;
-      scrollAmount += scrollStep;
-      if (scrollAmount >= carousel.scrollWidth - carousel.clientWidth) {
-        scrollAmount = 0;
-      }
-      carousel.scrollLeft = scrollAmount;
-    };
-
-    const interval = setInterval(autoScroll, 30);
-    return () => clearInterval(interval);
-  }, []);
+  const filteredPosts = blogPosts.filter((p) => p.id !== currentPostId);
+  const loopedPosts = [...filteredPosts, ...filteredPosts]; // duplicate for smooth loop
 
   return (
     <div>
       <h2 className="carousel-title">Still Not Done? More Sass Here ↓</h2>
-      <div className="carousel" ref={carouselRef}>
-        {blogPosts
-          .filter((p) => p.id !== currentPostId)
-          .slice(0, 6)
-          .map((item) => (
+      <div className="carousel">
+        <div className="carousel-track">
+          {loopedPosts.slice(0, 12).map((item, index) => (
             <Link
               to={`/blog/${item.id}`}
-              key={item.id}
+              key={`${item.id}-${index}`}
               className="carousel-item"
             >
               <img src={item.image} alt={item.title} />
               <h3>{item.title}</h3>
             </Link>
           ))}
+        </div>
       </div>
     </div>
   );
 }
 
 export default RelatedCarousel;
-// In RelatedCarousel.js, we created a new component that displays a carousel of related blog posts. We used the useRef hook to get a reference to the carousel element and the useEffect hook to automatically scroll the carousel horizontally.
